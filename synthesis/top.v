1f5b6eee14032e808c477003abb63ac0d29b50b8
Yann Sionneau <yann@minet.net>
2010-01-31 21:30:21 +0100

    modified top module to support usart I/O

--- synthesis/top.v
+++ synthesis/top.v
@@ -1,5 +1,7 @@
-module top(input clock, input reset, output led, output led2, output led3);
+module top(input clock, input reset, output led, output led2, output led3, input UART_TXD, output UART_RXD);
 
-generator g1 (clock, reset, led);
+generator g1 (clock, reset, led, led3, UART_RXD);
+
+assign led2 = UART_TXD;
 
 endmodule
